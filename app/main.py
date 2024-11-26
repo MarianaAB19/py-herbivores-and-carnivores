b1fa7a58068800f6f1711d136f3c53cfae4e9490
Mariana <mariana.antoniak.babish@gmail.com>
2024-11-26 16:31:58 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
+from typing import Union
+
+
 class Animal:
+    """stores all alive animals"""
     alive = []
 
     def __init__(self, name: str, health: int = 100) -> None:
@@ -9,29 +13,35 @@ class Animal:
             Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{" \
-               f"Name: {self.name}, " \
-               f"Health: {self.health}, " \
-               f"Hidden: {self.hidden}" \
-               f"}}"
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
     def beast_die(self) -> None:
+        """Removes the animal from Animal.alive list
+        if its health is 0 or less"""
         if self.health <= 0:
             Animal.alive.remove(self)
 
 
 class Herbivore(Animal):
     def hide(self) -> None:
+        """Changes the hidden property of the beast to the opposite value"""
         self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
-    def bite(self, animal: Herbivore) -> int | str:
+    def bite(self, animal: Herbivore) -> Union[int, str]:
+        """Takes a herbivore object and decreases the object's health by 50,
+        does not work if it is another сarnivore,
+        or the herbivore is currently hiding"""
         if isinstance(animal, Carnivore):
             return animal.health
         if animal.hidden is False:
             animal.health -= 50
             animal.beast_die()
         else:
-            return "Carnivore cannot bite hidden herbivore"
             animal.hide()
+            return "Carnivore cannot bite hidden herbivore"
