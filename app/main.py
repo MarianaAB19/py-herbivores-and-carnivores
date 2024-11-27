c189c3c9d87a8f5bc07b7c6e0e729b221c1969b4
Mariana <mariana.antoniak.babish@gmail.com>
2024-11-27 14:41:08 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
-from typing import Union
-
-
 class Animal:
     """stores all alive animals"""
     alive = []
@@ -9,8 +6,7 @@ class Animal:
         self.name = name
         self.health = health
         self.hidden = False
-        if self.health > 0:
-            Animal.alive.append(self)
+        Animal.alive.append(self)
 
     def __repr__(self) -> str:
         return (
@@ -18,12 +14,6 @@ class Animal:
             f"Health: {self.health}, "
             f"Hidden: {self.hidden}}}"
         )
-
-    def beast_die(self) -> None:
-        """Removes the animal from Animal.alive list
-        if its health is 0 or less"""
-        if self.health <= 0:
-            Animal.alive.remove(self)
 
 
 class Herbivore(Animal):
@@ -33,15 +23,11 @@ class Herbivore(Animal):
 
 
 class Carnivore(Animal):
-    def bite(self, animal: Herbivore) -> Union[int, str]:
+    def bite(self, animal: Herbivore) -> None:
         """Takes a herbivore object and decreases the object's health by 50,
         does not work if it is another сarnivore,
         or the herbivore is currently hiding"""
-        if isinstance(animal, Carnivore):
-            return animal.health
-        if animal.hidden is False:
+        if not animal.hidden and isinstance(animal, Herbivore):
             animal.health -= 50
-            animal.beast_die()
-        else:
-            animal.hide()
-            return "Carnivore cannot bite hidden herbivore"
+            if animal.health <= 0:
+                Animal.alive.remove(animal)
